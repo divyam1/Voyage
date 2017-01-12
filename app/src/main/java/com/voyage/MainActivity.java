4c106c5a9527954c50e482f8cf20a5aef2b4153b
divyam1 <divyam.upneja@gmail.com>
2017-01-13 02:35:56 +0530

    button added

--- app/src/main/java/com/voyage/MainActivity.java
+++ app/src/main/java/com/voyage/MainActivity.java
@@ -2,12 +2,17 @@ package com.voyage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.bt);
     }
+
 }
